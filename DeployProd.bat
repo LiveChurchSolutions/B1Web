d217d6772bd20a42d38fb5c1bdb824338550c6fb
Jeremy Zongker <jeremy@trilitech.com>
2021-12-08 22:03:16 -0600

    Removed batch files

--- DeployProd.bat
+++
@@ -1,4 +0,0 @@
-call SET REACT_APP_STAGE=prod
-call npm run build
-call aws s3 sync build/ s3://b1-web
-call aws cloudfront create-invalidation --distribution-id E18QK6HIZFU4NR --paths /*
--- DeployStaging.bat
+++
@@ -1,4 +0,0 @@
-call SET REACT_APP_STAGE=staging
-call npm run build
-call aws s3 sync build/ s3://staging-b1-web
-call aws cloudfront create-invalidation --distribution-id E2LBLBU6S37IDV --paths /*
